35d7290435b6bad0bd876c89efa669567d1d98d8
ベルンカステル <98995319+Witch-Bernkastel@users.noreply.github.com>
2025-05-08 13:05:24 -0300

    itemInfo_ptbr.lua

---
+++ System/itemInfo_ptbr.lua
@@ -0,0 +1,182 @@
+-- Criado e atualizado em: https://github.com/Witch-Bernkastel/RObrazilianRE/
+-- Este arquivo pode ser distribuído, usado e modificado livremente
+-- Última atualzação: 2025 05 08
+
+tbl = {
+	[0] = {
+		Server = "jRO",
+		unidentifiedDisplayName = "",
+		unidentifiedResourceName = "",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "",
+		identifiedResourceName = "",
+		identifiedDescriptionName = {
+			""
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[501] = {
+		unidentifiedDisplayName = "Poção Vermelha",
+		unidentifiedResourceName = "빨간포션",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Poção Vermelha",
+		identifiedResourceName = "빨간포션",
+		identifiedDescriptionName = {
+			"Poção feita de ervas vermelhas.",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 45 a 65 de HP.^000000",
+			"--------------------------",
+			"Peso: ^7777777^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[502] = {
+		unidentifiedDisplayName = "Poção Laranja",
+		unidentifiedResourceName = "주홍포션",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Poção Laranja",
+		identifiedResourceName = "주홍포션",
+		identifiedDescriptionName = {
+			"Poção feita de ervas vermelhas e amarelas. ",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 105 a 145 de HP.^000000",
+			"--------------------------",
+			"Peso: ^77777710^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[503] = {
+		unidentifiedDisplayName = "Poção Amarela",
+		unidentifiedResourceName = "노란포션",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Poção Amarela",
+		identifiedResourceName = "노란포션",
+		identifiedDescriptionName = {
+			"Poção feita de ervas amarelas. ",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 175 a 235 de HP.^000000",
+			"--------------------------",
+			"Peso: ^77777713^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[504] = {
+		unidentifiedDisplayName = "Poção Branca",
+		unidentifiedResourceName = "하얀포션",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Poção Branca",
+		identifiedResourceName = "하얀포션",
+		identifiedDescriptionName = {
+			"Poção feita de ervas brancas. ",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 325 a 405 de HP.^000000",
+			"--------------------------",
+			"Peso: ^77777715^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[505] = {
+		unidentifiedDisplayName = "Poção Azul",
+		unidentifiedResourceName = "파란포션",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Poção Azul",
+		identifiedResourceName = "파란포션",
+		identifiedDescriptionName = {
+			"Poção feita de ervas azuis. ",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 40 a 60 de SP.^000000",
+			"--------------------------",
+			"Peso: ^77777715^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[506] = {
+		unidentifiedDisplayName = "Poção Verde",
+		unidentifiedResourceName = "초록포션",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Poção Verde",
+		identifiedResourceName = "초록포션",
+		identifiedDescriptionName = {
+			"Poção feita de ervas verdes. ",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRemove Envenenamento, Silêncio, Cegueira, Caos e Alucinação.^000000",
+			"--------------------------",
+			"Peso: ^7777777^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[507] = {
+		unidentifiedDisplayName = "Erva Vermelha",
+		unidentifiedResourceName = "빨간허브",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Erva Vermelha",
+		identifiedResourceName = "빨간허브",
+		identifiedDescriptionName = {
+			"Erva de valor medicinal na cor vermelha.",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 18 a 28.^000000",
+			"--------------------------",
+			"Peso: ^7777773^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[508] = {
+		unidentifiedDisplayName = "Erva Amarela",
+		unidentifiedResourceName = "노란허브",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Erva Amarela",
+		identifiedResourceName = "노란허브",
+		identifiedDescriptionName = {
+			"Erva de valor medicinal na cor amarela.",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 38 a 58 de HP.^000000",
+			"--------------------------",
+			"Peso: ^7777775^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+	[509] = {
+		unidentifiedDisplayName = "Erva Branca",
+		unidentifiedResourceName = "하얀허브",
+		unidentifiedDescriptionName = { "" },
+		identifiedDisplayName = "Erva Branca",
+		identifiedResourceName = "하얀허브",
+		identifiedDescriptionName = {
+			"Erva de valor medicinal na cor branca.",
+			"Dê um clique duplo no item para usar.",
+			"--------------------------",
+			"^0000ffRecupera cerca de 75 a 115 de HP.^000000",
+			"--------------------------",
+			"Peso: ^7777777^000000"
+		},
+		slotCount = 0,
+		ClassNum = 0,
+		costume = false
+	},
+}
